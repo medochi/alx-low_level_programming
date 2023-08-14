c7e1881f2c8df4ecf104c366a0830b9702512f3c
medochi <mohammed.sayed05@eng-st.cu.edu.eg>
2023-08-14 11:33:38 -0700

    lol

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,8 +10,8 @@
  * Return: void
 */
 
- void init_dog(struct dog *d, char *name, float age, char *owner){
-
+ void init_dog(struct dog *d, char *name, float age, char *owner)
+{
 	 if(d){
 		 d->name = name ;
 		 d->age = age ;
