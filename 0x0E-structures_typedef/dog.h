7a430aa2c5b4af6175ffa9e7fefd12ba6a30e3c9
medochi <mohammed.sayed05@eng-st.cu.edu.eg>
2023-08-14 11:09:42 -0700

    aweasome

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,19 @@
 #ifndef DOG
 #define DOG
 
+/**
+ * struct dog - dog class
+ * @name: Name of the dog 
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Oner of the dog
+ */
 
 
 
-struct dog {
+struct dog
+{
 	float age ;
 	char *name;
 	char *owner;
