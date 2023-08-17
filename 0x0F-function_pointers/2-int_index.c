25304004ebe830ca5b2fa90d59261b3150c23621
medochi <mohammed.sayed05@eng-st.cu.edu.eg>
2023-08-17 16:03:24 -0700

    new code

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,36 @@
+include "funnction_pointers.h"
+
+/**
+ * int_index - function that checks
+ *
+ * @size: size of array
+ * @array: array rhat we itirate in
+ * @cmp: function
+ *
+ *
+ * Return: int
+ *
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array && cmp)
+
+	{
+		int i;
+
+		for (i = 0 ; i < size ; i++)
+
+		{
+			bool what;
+
+			what = cmp(array[i]);
+			if (bool)
+
+			{
+				return (i);
+			}
+		}
+
+	}
+}
