7aa360507204da13bf5e88a4b21fc29c5241f28b
medochi <mohammed.sayed05@eng-st.cu.edu.eg>
2023-08-17 15:42:24 -0700

    new code

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,9 +3,9 @@
 /**
  * array_iterator - a function that puts array into another function
  *
- * @array; a pointer to array
  * @size: size of the array
  * @action: function that take the values of the array
+ * @array: an array
  *
  * Return: void
  *
@@ -13,6 +13,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t  i;
+
 	for (i = 0 ; i < size ; i++)
 	{
 
