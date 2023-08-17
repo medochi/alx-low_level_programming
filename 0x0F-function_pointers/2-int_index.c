3ae868d90f0bf1a440574c29beba1830f376a954
medochi <mohammed.sayed05@eng-st.cu.edu.eg>
2023-08-17 16:12:12 -0700

    new code

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stdbool.h>
+
 /**
  * int_index - function that checks
  *
@@ -22,7 +22,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		for (i = 0 ; i < size ; i++)
 
 		{
-			bool what;
+			int what;
 
 			what = cmp(array[i]);
 			if (what)
